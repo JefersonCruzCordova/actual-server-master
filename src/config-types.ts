1aeb8697a51c5a65f263748d02dbe1bb258f9dd1
Jeferson Cruz Cordova <jeferson.cruz@unmsm.edu.pe>
2023-10-23 12:05:53 -0500

    Se agrega el proyecto

---
+++ src/config-types.ts
@@ -0,0 +1,19 @@
+import { ServerOptions } from 'https';
+
+export interface Config {
+  mode: 'test' | 'development';
+  port: number;
+  hostname: string;
+  serverFiles: string;
+  userFiles: string;
+  webRoot: string;
+  https?: {
+    key: string;
+    cert: string;
+  } & ServerOptions;
+  upload?: {
+    fileSizeSyncLimitMB: number;
+    syncEncryptedFileSizeLimitMB: number;
+    fileSizeLimitMB: number;
+  };
+}
